63d9bd37f57c6fa0a63bbf10dcb8cff1228f4955
Rikson <noskir123@yahoo.co.jp>
2017-07-10 14:01:54 +0800

    Update index.js to use npm

---
+++ src/Index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Grid } from 'react-bootstrap';
+import Command from './Command/Command';
+
+class Index extends React.Component {
+  render(){
+    return (
+      <Grid>
+        <h1>Browxin</h1>
+        <p className="lead hidden-xs">Open Source, Multiple Search, Commands, Custom Widgets</p>
+        <Command />
+      </Grid>
+    );
+  }
+}
+
+export default Index;
--- src/app.js
+++ src/app.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap';
-import { Grid } from 'react-bootstrap';
+import Index from './Index.js';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import Command from './Command/Command';
 import reducer from './reducers.js';
-
-class Index extends React.Component {
-  render(){
-    return (
-      <Grid>
-        <h1>Browxin</h1>
-        <p className="lead hidden-xs">Open Source, Multiple Search, Commands, Custom Widgets</p>
-        <Command />
-      </Grid>
-    );
-  }
-}
 
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
